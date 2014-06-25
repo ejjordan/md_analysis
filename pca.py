85dbb589bd837ffa3e4b7368cb6ae07d1f435191
ejjordan <e.jjordan12@gmail.com>
2014-06-25 11:01:17 -0400

    mess

--- pca.py
+++ pca.py
@@ -1,5 +1,7 @@
 from scipy import linalg as la
 from MDAnalysis import *
+import MDAnalysis
+import MDAnalysis.analysis as analysis
 import numpy as np
 import os
 
@@ -11,14 +13,18 @@ class PCA(object):
         self.trajectory = trajectory
         self.targetdir = targetdir
         self.selection = selection
-        trajectorybase = os.path.splitext(os.path.basename(trajectory))[0]
-        output = trajectorybase + '_pca.dat'
+        trajbase = os.path.splitext(os.path.basename(trajectory))[0]
+        output = trajbase + '_pca.dat'
         self.output = os.path.join(self.targetdir, output)
         self.universe = Universe(topology, trajectory)
     
     def make_covariance(self):
-
         selection = self.universe.selectAtoms(self.selection)
+        #reference = self.universe.selectAtoms(self.selection)
+        ref="ref.gro"
+        gro_writer=MDAnalysis.coordinates.GRO.GROWriter(self.targetdir+ref)
+        gro_writer.write(self.universe,0)
+        reference=Universe(self.targetdir+ref).selectAtoms(self.selection)
         num_atoms=selection.numberOfAtoms()
         num_frames=self.universe.trajectory.numframes
         dof=num_atoms*3
@@ -29,7 +35,8 @@ class PCA(object):
         num_confs=0
         coordsum=np.zeros(dof)
         for ts in self.universe.trajectory:
-            frame = ts.frame
+            R=analysis.align.rotation_matrix(selection.coordinates(),reference.coordinates())
+            selection.atoms.rotate(R[0])
             coords=selection.coordinates().flatten()
             coordsum += coords
             cov +=np.outer(coords,coords)
