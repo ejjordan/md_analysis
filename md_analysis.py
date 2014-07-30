7c6bf5001f87f85c8d5be2252b0d5612219c88a4
ejjordan <e.jjordan12@gmail.com>
2014-07-30 09:41:39 -0400

    made classes

--- md_analysis.py
+++ md_analysis.py
@@ -4,77 +4,96 @@ from MDAnalysis.analysis.align import *
 import MDAnalysis.analysis.hbonds
 import os
 import sys
-
-#MAY need these to take periodicity into account for atomselect
-#MDAnalysis.core.flags['use_periodic_selections'] = True
-#MDAnalysis.core.flags['use_KDTree_routines'] = False
-
-path="/home/joe/egfr/g719s/inactive-monomer/simulate/"
-trj_file="short.xtc"
-gro_file="md.part0001.gro"
-frame0="frame0.gro"
-
-trj=Universe(path+gro_file,path+trj_file)
-gro_writer=MDAnalysis.coordinates.GRO.GROWriter(path+frame0)
-gro_writer.write(trj,0)
-ref=Universe(path+frame0)
-#rms_fit_trj(trj,ref,{'mobile':'backbone', 'reference':'backbone'},
-#            filename=None,rmsdfile=path+"fit.dat")
-#os.remove(path+"rmsfit_"+trj_file)
-
-protein=trj.selectAtoms("protein")
-ions = trj.selectAtoms("type NA") + trj.selectAtoms("type CL")
-notwater=trj.selectAtoms("not resname SOL")
-
-sys.path.insert(0,"/home/joe/git/python_mdanalysis/mdanalysis/package/MDAnalysis/analysis")
-
-SASA={}
 import Bio.PDB as bp
 import numpy as np
-for ts in trj.trajectory:
-    writer=MDAnalysis.Writer("tmp.pdb")
-    writer.write(protein)
-    writer.close()
-    parser=bp.PDBParser()
-    structure=parser.get_structure('tmp','tmp.pdb')
-    dssp=bp.DSSP(structure[0],'tmp.pdb',"/usr/bin/mkdssp")
-    for key in dssp.keys():
-#        print key
-        resnum=dssp[key][0]
-        sasa=dssp[key][2]
-        #print resnum.id[1],sasa
-        if resnum.id[1] in SASA:
-            SASA[resnum.id[1]].append(sasa)
-        else:
-            SASA[resnum.id[1]]=[sasa]
-#print np.mean(SASA[697])
-#print np.std(SASA[697],ddof=1)
-count=0
-for key in SASA:
-    print protein.resnames()[count],key,np.mean(SASA[key]),np.std(SASA[key],ddof=1)
-    count+=1
 
 """
-import joe_hbonds as joe
-hbonds=joe.HydrogenBondAnalysis(trj, 'protein', 'protein',
-                                update_selection1=True, 
-                                update_selection2=True, 
-                                detect_hydrogens='distance', 
-                                distance=3.00, angle=160.0, 
-                                distance_type="heavy")
-hbonds.run()
-hbonds_occupancy=hbonds.count_by_type()
-print hbonds_occupancy[0]
-#hbonds_occupancy.generate_table()
-#hbonds_occupancy.save_table(path+"hbonds-occupancy.pickle")
+This script contains functions useful for analyzing MD trajectories
+to determine if mutations (or other perturbations) cause a
+structural change in a protein that may bias it towards a
+different state than the wild type (or non-perturbed) system.
 
-
-sel_basic = "(resname ARG or resname LYS) and (name NH* or name NZ)"
-sel_acidic = "(resname ASP or resname GLU) and (name OE* or name OD*)"
-basic=trj.selectAtoms(sel_basic)
-acidic=trj.selectAtoms(sel_acidic)
-# set up analysis of native contacts ("salt bridges"); with distance <6 A
-CA1 = MDAnalysis.analysis.contacts.ContactAnalysis1(trj, selection=(sel_acidic, sel_basic), refgroup=(acidic, basic), radius=3.0, outfile=path+"qsalt.dat")
-CA1.run(force=True)
-CA1.plot(path+"plot.pdf")
+It is currently a work in progress!
 """
+
+
+class analyzer(object):
+
+    def __init__(self, topology, trajectory, path=os.getcwd()):
+        self.path = path + '/'
+        self.trajectory = trajectory
+        self.topology = topology
+        self.universe = MDAnalysis.Universe(self.path + self.topology,
+                                            self.path + self.trajectory)
+
+    def get_rms(self, ref_name="frame0.gro"):
+        self.ref_name = ref_name
+        gro_writer=MDAnalysis.coordinates.GRO.GROWriter(self.path
+                                                        + self.ref_name)
+        gro_writer.write(self.universe,0)
+        self.ref_struct=Universe(self.path+self.ref_name)
+        rms_fit_trj(self.universe,self.ref_struct,
+                    {'mobile':'backbone', 'reference':'backbone'},
+                    filename=None,rmsdfile=self.path + "fit.dat")
+        os.remove(sys.path + "rmsfit_" + self.trajectory) #remove fitted traj
+
+
+    def get_sasa(self, dssp_loc="/usr/bin/mkdssp"):
+        self.dssp_loc = dssp_loc
+        SASA={}
+        protein=self.universe.selectAtoms("protein")
+        for ts in self.universe.trajectory:
+            writer=MDAnalysis.Writer("tmp.pdb")
+            writer.write(protein)
+            writer.close()
+            parser=bp.PDBParser()
+            structure=parser.get_structure('tmp','tmp.pdb')
+            dssp=bp.DSSP(structure[0],'tmp.pdb',self.dssp_loc)
+            for key in dssp.keys():
+                resnum=dssp[key][0]
+                sasa=dssp[key][2]
+                if resnum.id[1] in SASA:
+                    SASA[resnum.id[1]].append(sasa)
+                else:
+                    SASA[resnum.id[1]]=[sasa]
+        count=0
+        for key in SASA:
+            print protein.resnames()[count],key,np.mean(SASA[key]),np.std(SASA[key],ddof=1)
+            count+=1
+
+
+    def get_salts(self):
+        ions = self.universe.selectAtoms("type NA") + self.universe.selectAtoms("type CL")
+        sel_basic = "(resname ARG or resname LYS) and (name NH* or name NZ)"
+        sel_acidic = "(resname ASP or resname GLU) and (name OE* or name OD*)"
+        basic=self.universe.selectAtoms(sel_basic)
+        acidic=self.universe.selectAtoms(sel_acidic)
+        #set up analysis of native contacts ("salt bridges"); with distance <6 A
+        CA1 = MDAnalysis.analysis.contacts.ContactAnalysis1(self.universe, selection=(sel_acidic, sel_basic), refgroup=(acidic, basic), radius=3.0, outfile=self.path+"qsalt.dat")
+        CA1.run(force=True)
+        CA1.plot(self.path+"plot.pdf")
+
+
+    def get_hbonds(self, distance=3.0, angle=160.0):
+        sys.path.insert(0,"/home/joe/git/python_mdanalysis/mdanalysis/package/MDAnalysis/analysis")
+        self.distance=distance
+        self.angle=angle
+        import joe_hbonds as joe
+        hbonds=joe.HydrogenBondAnalysis(self.universe, 'protein', 'protein',
+                                        update_selection1=True, 
+                                        update_selection2=True, 
+                                        detect_hydrogens='distance', 
+                                        distance = self.distance,
+                                        angle = self.angle, 
+                                        distance_type="heavy")
+        hbonds.run()
+        hbonds_occupancy=hbonds.count_by_type()
+        print hbonds_occupancy[0]
+        hbonds_occupancy.generate_table()
+        hbonds_occupancy.save_table(self.path+"hbonds-occupancy.pickle")
+
+    def make_surface_graph(self, CaCa_dist=0.6, Ca1Cb2_dist=0.75,
+                           Ca2Cb1_dist=0.75, dihedral=0.35):
+        vector_list=[self.universe.selectAtoms("name CA").coordinates(),
+                     self.universe.selectAtoms("name CB").coordinates()]
+        print vector_list
