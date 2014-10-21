41fe9785b77a595ae6bb4f1681ca308b0063ee0b
ejjordan <e.jjordan12@gmail.com>
2014-10-20 20:01:29 -0400

    updated graphs.py and wrote a wrapper, wrap_graphy.py

---
+++ wrap_graph.py
@@ -0,0 +1,39 @@
+import sys
+sys.path.insert(0,'/home/joe/git/md_analysis/')
+import graphs as gp
+import MDAnalysis as mda
+import numpy as np
+
+mutation_list=['wt','f1174l','f1245c','f1245v','g1128a','i1170n','i1170s',
+               'i1171n','m1166r','r1192p','r1275q','t1151m','y1278s']
+directory='./140ns-dry/'
+start_res=1084
+c_dist_list=[4, 4.5, 5, 5.5, 6]
+non_c_dist_list=[4, 4.5, 5, 5.5, 6]
+dt=350
+is_psf=True
+
+for c_dist in c_dist_list:
+    for non_c_dist in non_c_dist_list:
+        for mutation in mutation_list:
+            print "analyzing {0}_{1}_{2}_{3} system".format(mutation,c_dist,
+                                                            non_c_dist,dt)
+            psf='alk.dry.{0}.psf'.format(mutation)
+            dcd='alk.140ns.aligned.dry.{0}.dcd'.format(mutation)
+            out_file_name="{0}_{1}_{2}_{3}".format(mutation,c_dist,non_c_dist,dt)
+            universe=mda.Universe(directory+psf,directory+dcd)
+            graf=gp.graphs(universe)
+            trajectory_graph=graf.trajectory_graph(start_res=start_res,C_distance=
+                                                   c_dist,non_C_distance=non_c_dist,
+                                                   skip=dt,psf=is_psf)
+            graf.analyze_trajectory_graph(trajectory_graph)
+            graph_dict=graf.graph_attribute_graphs(trajectory_graph,display=False,
+                                                   write_file=out_file_name+'.png')
+            fp=open(out_file_name+'.dat','w')
+            fp.write("start_res={0}\tC_distance={1}\tnon_C_distance={2}\tskip={3}\n\n"
+                     .format(start_res,c_dist,non_c_dist,dt))
+            for key in graph_dict:
+                mean=np.mean(graph_dict[key])
+                std=np.std(graph_dict[key])
+                fp.write("{0}\t{1}\t{2}\n{3}\n\n".format(key,mean,std,graph_dict[key]))
+            fp.close()
